7e45955af869119dec4ef6ddbdf4bd09116471ef
Joao Pedro Rodrigues Rocha <rocha.pedro.joao@gmail.com>
2022-02-24 17:58:19 -0300

    Fiz até o exercicio 6

---
+++ Idade.java
@@ -0,0 +1,17 @@
+import javax.swing.JOptionPane;
+public class Idade {
+    public static void main(String[] args) {
+        //declaração de variaveis
+        double idadeAnos, idadeDias;
+
+        //entrada de dados
+        idadeAnos = Double.parseDouble(JOptionPane.showInputDialog("Digite a idade a ser convertida(Em Anos)"));
+
+        //processamento
+        idadeDias = idadeAnos * 365;
+
+        //saida
+        String s = String.format("A idade digitada em dias é: %.2f", idadeDias);
+        JOptionPane.showMessageDialog(null, s);
+    }
+}
--- Triangulo.java
+++ Triangulo.java
@@ -1,5 +1,18 @@
+import javax.swing.JOptionPane;
 public class Triangulo {
     public static void main(String[] args) {
         //criação de variaveis
+        double base, alt, res;
+
+        //entrada de dados
+        base = Double.parseDouble(JOptionPane.showInputDialog("Digite a base do triângulo:"));
+        alt = Double.parseDouble(JOptionPane.showInputDialog("Digite a altura do triângulo"));
+
+        //processamento
+        res = (base * alt) / 2;
+
+        //saida
+        String s = String.format("A área do triângulo é: %.2f", res);
+        JOptionPane.showMessageDialog(null, s);
     }
 }
